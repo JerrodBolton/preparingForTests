e46387a270bbbfd1a5efdb7e57d68d64f8d14931
Jerrod <JerrodBolton2000@gmail.com>
2022-03-29 13:12:06 -0500

    two queation

---
+++ main.js
@@ -0,0 +1,32 @@
+// alert("Why do I feel the way that I am feel");
+
+// let ceil = Math.ceil(-100.5); //This is something that round up 
+// let floor = Math.floor(100.5); //This is something that round down 
+// let round = Math.round(100.5); //This is something that round up 
+
+// console.log(`Ceil:${ceil} Floor:${floor} Round:${round}`);
+
+// var date = new Date(2017, 0);
+// the first num is  year 
+// the first num is number month
+
+
+// Create a function that takes the age in years and returns the age in days.
+
+
+// function calcAge(age) {
+//  let days = age * 365 
+//  return(days)
+
+// //  return("hello edabit.com");
+// }
+
+// console.log(calcAge(65));
+
+// Write a function that converts hours into seconds.
+
+function howManySeconds(hours) {
+	
+}
+
+
